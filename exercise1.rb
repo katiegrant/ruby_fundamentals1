dfad68999d137aacd044b38295582452868bfcd9
Katie Grant <kate@kategrant.ca>
2015-06-10 17:11:56 -0400

    second exercise file ready to be uploaded

--- exercise1.rb
+++ exercise1.rb
@@ -5,3 +5,38 @@ puts 3
 puts 2 != 3
 
 end
+
+
+
+# Strings literal
+
+puts 'Hello world'
+puts "Hello world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+
+# Double and Single Quotes
+
+puts "Katie Grant"
+name = gets.chomp
+puts "Your name is #{name}"
+
+
+# String Interpolations
+
+a = 20
+b = 65
+puts "The value of a is #{a}." # The value of a is 20.
+puts "The value of a is #{b}." # The value of a is 65.
+puts "The value of a plub b is #{a + b}." # The value of a plus b is 85.
+
+# Escape Sequences
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbuy world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+# Booleans 
---
+++ exercise2.rb
@@ -0,0 +1,26 @@
+# How would you calculate a good tip for a $55 meal. 
+# Average slightly upscale restaurant servers recieve a 20% tip.
+
+#option one
+first_amount, second_amount = 55, 1.20
+first_amount * second_amount
+
+# option two
+puts 55 * 1.20
+end
+
+
+# Adding a string
+puts "Thank you for excellent service!"
+
+
+# Here is the integer that prints on screen using puts
+
+bunny = 45628
+elf = 7839
+
+puts "Every year #{bunny * elf} aliens secretly land on planet earth."
+
+# What is the value?
+
+puts (true && false) || (false && true) || !(false && false)
